6b51083b2af57735cde36fe211a5bcc89e386a4d
João Nakano <138939556+joaonakano@users.noreply.github.com>
2024-10-30 09:37:09 -0300

    new-files-upload

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    private CharacterController characterController;
+
+    public float Speed = 5f;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        characterController.Move(move * Time.deltaTime * Speed);
+    }
+}
