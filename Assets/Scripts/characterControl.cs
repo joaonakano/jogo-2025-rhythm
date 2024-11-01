88e367dadebc02cab84f856bd25421772dd9f93d
Boris Philipp <engs-borisluz@camporeal.edu.br>
2024-11-01 19:36:15 -0300

    colisoes implementadas e personagem com movimento

--- Assets/Scripts/characterControl.cs
+++ Assets/Scripts/characterControl.cs
@@ -1,16 +1,43 @@
 using UnityEngine;
 
-public class characterControl : MonoBehaviour
+[RequireComponent(typeof(Rigidbody))]
+public class MovimentoPersonagemPlano : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float velocidade = 5f; // Velocidade de movimento no plano XZ
+    public float forcaPulo = 5f; // Força do pulo
+    private Rigidbody rb;
+    private bool estaNoChao;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        rb.useGravity = true;
+        rb.isKinematic = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        // Movimentação no plano XZ
+        float movimentoHorizontal = Input.GetAxis("Horizontal");
+        float movimentoVertical = Input.GetAxis("Vertical");
+        Vector3 movimento = new Vector3(movimentoHorizontal, 0, movimentoVertical).normalized * velocidade;
+        rb.MovePosition(rb.position + movimento * Time.fixedDeltaTime);
+
+        // Verifica se o jogador pressiona o botão de pulo e se está no chão
+        if (Input.GetButtonDown("Jump") && estaNoChao)
+        {
+            rb.AddForce(Vector3.up * forcaPulo, ForceMode.Impulse);
+            estaNoChao = false; // Impede que pule novamente enquanto estiver no ar
+        }
+    }
+
+    // Verifica se o personagem toca o chão
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Confirma se a colisão é com o chão usando a tag "Chao"
+        if (collision.gameObject.CompareTag("Chao"))
+        {
+            estaNoChao = true;
+        }
     }
 }
